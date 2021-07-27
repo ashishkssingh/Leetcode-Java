5aeff7638d5fc5ca8f769519606f87bbd1cf5818
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-07-27 17:38:26 -0400

    Problem #141
    Linked List Cycle

---
+++ Algorithms/LinkedListCycle/LinkedListCycle.java
@@ -0,0 +1,85 @@
+package Algorithms.LinkedListCycle;
+
+/**
+ * 
+ * 141. Linked List Cycle
+ * 
+ * Given head, the head of a linked list, determine if the linked list has a
+ * cycle in it.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is connected
+ * to. Note that pos is not passed as a parameter.
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1 Output: true Explanation: There is a cycle
+ * in the linked list, where the tail connects to the 1st node (0-indexed).
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0 Output: true Explanation: There is a cycle in
+ * the linked list, where the tail connects to the 0th node. Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1 Output: false Explanation: There is no cycle in
+ * the linked list.
+ * 
+ * 
+ * Constraints:
+ * 
+ * The number of the nodes in the list is in the range [0, 104]. -105 <=
+ * Node.val <= 105 pos is -1 or a valid index in the linked-list.
+ * 
+ * 
+ * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ */
+public class LinkedListCycle {
+    static class ListNode {
+
+        Integer val;
+        ListNode next;
+
+        ListNode(Integer x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ListNode makeLinkedList(Integer[] a) {
+        ListNode head = new ListNode(a[0]);
+        ListNode tail = head;
+        for (int i = 1; i < a.length; i++) {
+            ListNode node = new ListNode(a[i]);
+            tail.next = node;
+            tail = tail.next;
+        }
+        tail.next = head;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[] { 3, 2, 0, -4 };
+        ListNode linkedList = makeLinkedList(arr);
+        System.out.println(hasCycle(linkedList));
+    }
+}
